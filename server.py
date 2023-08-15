98db988cad164507538d0fa9c87ea7779e39a330
Harish Sankaranarayanan S <harish251102@gmail.com>
2023-08-15 12:51:27 +0530

    working

--- server.py
+++ server.py
@@ -1,35 +1,29 @@
-from flask import Flask, request , jsonify
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello World!'
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    data = request.json  # Assuming you're sending JSON data in the request
-    num1 = data.get('num1')
-    num2 = data.get('num2')
-    
-    if num1 is None or num2 is None:
-        return jsonify(error="Both 'num1' and 'num2' must be provided."), 400
-    
-    result = num1 + num2
-    return jsonify(result=result)
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data = request.json  # Assuming you're sending JSON data in the request
-    num1 = data.get('num1')
-    num2 = data.get('num2')
-    
-    if num1 is None or num2 is None:
-        return jsonify(error="Both 'num1' and 'num2' must be provided."), 400
-    
-    result = num1 - num2
-    return jsonify(result=result)
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
